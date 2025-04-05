10ec7cbd2187157f028ec1b25c92349489574252
CoderShivam <shivamkumargupta3456@gmail.com>
2025-04-05 23:49:57 +0530

    Update Login.jsx

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
  const handleGoogleLogin = () => {
   try {
     // Redirect user to backend Google OAuth endpoint
-    window.location.href = "http://localhost:4000/auth/google/";
+    window.location.href = "https://job-rashpi-2-frontend.onrender.com/auth/google/";
   } catch (err) {
     console.error("Error during Google login:", err);
   }
